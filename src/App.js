f6ae7e2e8b96e434b0a6b79afb743add2a83cd12
Sagi Mines <sagi1236@gmail.com>
2022-07-10 15:41:34 +0300

    Added 'nice-waves' background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import { useState } from 'react';
 import getTheUsers from './getTheUsers';
 import PageStructure from './PageStructure';
+import waves from 'nice-waves';
+import './App.css';
 
 function App() {
+  //waves background
+  const opts = {
+    fills: ['rgba(29, 169, 30, 0.65)', 'rgba(75, 187, 91, 0.5)'],
+    flowRate: 5,
+    swayRate: 2.95,
+    wavelength: 10,
+    curviness: 0.57,
+    randomHeight: 0.42,
+    randomSwayRate: 0.31,
+    randomOffset: 0.51,
+  };
+
+  waves(opts).mount();
   //Sort the users by score
   const order = (a, b) => {
     return b.score - a.score;
